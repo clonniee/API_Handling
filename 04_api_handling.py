ed126dbda868da58bbfa669ab55641dd4cf42399
clonniee <hby110055@gmail.com>
2025-03-19 18:34:41 +0530

    Add files via upload

---
+++ 04_api_handling.py
@@ -0,0 +1,32 @@
+#added random with own knowledge hence some improvement
+# add virtual environment for pc safety ig
+
+import requests 
+import random 
+
+def jokes():
+    url = "https://api.freeapi.app/api/v1/public/randomjokes"
+    deta = requests.get(url)
+    data_in_json = deta.json()
+    if data_in_json["success"] and "data" in data_in_json :
+        data_from_api = data_in_json["data"]
+        joke = data_from_api["data"]
+        listt = [x for x in joke]
+        number  = random.choice(listt)
+        content = number["content"]
+        jock = print(str(content))
+        return jock  
+    
+    else :
+        Exception("FAILtoFETCH")
+
+
+def main():
+    try :
+        jokes()
+    except Exception as e :
+        print(str(e))       
+
+
+if __name__ == "__main__" :
+    main()
